f4a1a48ee925f2febd780ad9d42d82438a6ad27f
FerrariRoma <85842907+wayneglik@users.noreply.github.com>
2021-10-19 18:51:58 +0900

    reorganized folders

---
+++ js/momentum/add.js
@@ -0,0 +1,15 @@
+console.log("니코는 선생님이다.");
+console.log('니코는 선생님이다.');
+// js도 변수 선언을 할 수 있음
+// const는 상수이다. 근데 변수를 선언할때
+// 이렇게 한다네? 흠..
+// C와 같이 "2"는 숫자가 아니다.
+const a=5, b=2;
+console.log(a+b);
+console.log(a-b);
+
+const name = "NICO";
+console.log("hello " + name);
+
+// 자바스크립트에서 변수를 선언할때 const를 사용하는 방법은
+// 반만 맞는 말이다. let을 쓸 수도 있기 때문이다.
