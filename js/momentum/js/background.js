73ff18d97c32030c6c4cdb8f8471ae2fb2d68daf
FerrariRoma <85842907+wayneglik@users.noreply.github.com>
2021-10-31 04:21:43 +0900

    review last assignment

--- js/momentum/js/background.js
+++ js/momentum/js/background.js
@@ -12,3 +12,7 @@ const bgImage = document.createElement("img");
 bgImage.src = `img/${chosenImage}`;
 
 document.body.appendChild(bgImage);
+
+// document.body.style.backgroundImage = `url(img/${randPick})`;
+// document.body.style.backgroundSize = `100%`;
+// document.body.style.backgroundSize = `100vh`;
